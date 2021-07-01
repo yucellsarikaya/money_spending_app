696a7d4a1b9b9a149b1cd582c0c4bd44e4a1cf7e
yucellsarikaya <2017141050@cumhuriyet.edu.tr>
2021-07-01 19:19:15 +0300

    Initial commit

---
+++ src/components/BasketItem.js
@@ -0,0 +1,20 @@
+function BasketItem({ product, item }) {
+    return (
+        <>
+			<li className="basket-item">
+				{product.title} <span>x {item.amount}</span>
+			</li>
+			<style jsx>{`
+              .basket-item {
+                padding-bottom: 10px;
+                font-size: 17px;
+              }
+              .basket-item span {
+                color: #999;
+              }
+			`}</style>
+		</>
+    )
+}
+
+export default BasketItem
